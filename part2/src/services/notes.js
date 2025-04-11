50828b325b578de06731f5227f0f27090c1b44a2
Emu123R <emma.k.s.riekkinen@gmail.com>
2025-04-11 16:03:41 +0300

    Update notes.js

--- part2/src/services/notes.js
+++ part2/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://localhost:3000notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
