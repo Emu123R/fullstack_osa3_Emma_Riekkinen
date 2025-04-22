7315bc2ce99284c546ea59b78a4955c0059e5fde
Emu123R <emma.k.s.riekkinen@gmail.com>
2025-04-22 12:01:12 +0300

    u

---
+++ node-palvelin/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { readFile } = require('fs').promises;
+const http = require('http');
+const app = express();
+
+app.get('/', async (req, res) => {
+    response.send( await readFile('home.html', 'utf8'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Palvelin kuuntelee: http://localhost:3000');
+});
+
